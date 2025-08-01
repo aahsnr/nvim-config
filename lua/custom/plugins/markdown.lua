36d7333e713eddc8a46aaa57208bd77e95dd9569
aahsnr <ahsanur041@proton.me>
2025-08-02 05:41:04 +0600

    first commit

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,12 @@
+-- lua/custom/plugins/markdown.lua
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+}
