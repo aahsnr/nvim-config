381ec9196642345186620b21d2e02a2ea9f10e23
aahsnr <ahsanur041@proton.me>
2025-08-10 15:12:57 +0600

    updated

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,5 +1,7 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.nix" },
-  { import = "lazyvim.plugins.extras.lang.python" },
+  -- { import = "lazyvim.plugins.extras.ui.dashboard-nvim" },
+  -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  -- { import = "lazyvim.plugins.extras.lang.json" },
+  -- { import = "lazyvim.plugins.extras.lang.nix" },
+  -- { import = "lazyvim.plugins.extras.lang.python" },
 }
